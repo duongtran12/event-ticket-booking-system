06cd1872e9594365248d8c878f198090e6767af5
duongtran12 <tranducduong2204@gmail.com>
2026-05-10 09:35:24 +0700

    Create Role entity and RoleName enum

---
+++ src/main/java/com/duong/eventticket/entity/Role.java
@@ -0,0 +1,23 @@
+package com.duong.eventticket.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+
+}
---
+++ src/main/java/com/duong/eventticket/entity/RoleName.java
@@ -0,0 +1,8 @@
+package com.duong.eventticket.entity;
+
+public enum RoleName {
+
+    ROLE_USER,
+    ROLE_ADMIN
+
+}
